f708ddcb7ea3f562857c4bace6833fbd7c57e336
Badmonky <hermann.sven.o@gmail.com>
2022-02-11 13:38:36 -0700

    fix metamask on mobile #3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ReplaySubject } from 'rxjs';
 import { AlertService } from './alert.service';
-import detectEthereumProvider from '@metamask/detect-provider';
+// import detectEthereumProvider from '@metamask/detect-provider';
 
 @Injectable({
   providedIn: 'root',
@@ -51,57 +51,60 @@ export class AuthService {
     console.log('Check!');
     this.connectedAccount = null;
 
-    let ethereum: any = window.ethereum;
-      if (ethereum && ethereum.isMetaMask) {
-        console.log('Check = 2');
-        this.web3Provider = ethereum;
+    // let ethereum: any = window.ethereum;
+    // const { ethereum } = window;
+    const ethereum = (window as any).ethereum;
+    if (ethereum && ethereum.isMetaMask) {
+      console.log('Ethereum successfully detected!');
+      this.web3Provider = ethereum;
 
-        try {
-          // Request account access
-          this.web3Provider.request({ method: 'eth_requestAccounts' }).then((accounts: any) => {
-            this.connectedAccount = accounts[0];
-          });
+      try {
+        // Request account access
+        this.web3Provider.request({ method: 'eth_requestAccounts' }).then((accounts: any) => {
+          this.connectedAccount = accounts[0];
+        });
 
-          this.web3Provider.on('accountsChanged', (accounts: any) => {
-            console.log('Check = 3');
-            this.connectedAccount = accounts[0];
+        this.web3Provider.on('accountsChanged', (accounts: any) => {
+          console.log('Check = 3');
+          this.connectedAccount = accounts[0];
+          setTimeout(() => {
+            window.location.reload();
+          }, 100)
+        });
+
+        this.web3Provider.on('chainChanged', (chainId: any) => {
+          console.log('Check = 4');
+          if (!this._checkChain(parseInt(chainId, 16))) {
             setTimeout(() => {
               window.location.reload();
             }, 100)
-          });
+            return;
+          }
+        });
 
-          this.web3Provider.on('chainChanged', (chainId: any) => {
-            console.log('Check = 4');
-            if (!this._checkChain(parseInt(chainId, 16))) {
-              setTimeout(() => {
-                window.location.reload();
-              }, 100)
-              return;
-            }
-          });
-
-          setTimeout(() => {
-            console.log('Check = 5');
-            if (!this._checkChain(parseInt(ethereum.chainId, 16))) {
-              this.connectedAccount = null;
-              this.handleError("Please connect to Polygon");
-            }
-          }, 100);
-        } catch (error) {
-          console.log('Check = 6');
-          this.connectedAccount = null;
-          this.handleError("Couldn't connect to MetaMask");
-        }
-        return;
+        setTimeout(() => {
+          console.log('Check = 5');
+          if (!this._checkChain(parseInt(ethereum.chainId, 16))) {
+            this.connectedAccount = null;
+            this.handleError("Please connect to Polygon");
+          }
+        }, 100);
+      } catch (error) {
+        console.log('Check = 6');
+        this.connectedAccount = null;
+        this.handleError("Couldn't connect to MetaMask");
       }
+      return;
+    } else {
       console.log('Check = 7');
       this.handleError("Couldn't connect to MetaMask");
       return;
     }
+  }
 
 
   connectToMetaMask() {
-    if (window.ethereum) {
+    if ((window as any).ethereum) {
       this.handleEthereum();
     } else {
       window.addEventListener('ethereum#initialized', this.handleEthereum, {
@@ -109,6 +112,7 @@ export class AuthService {
       });
       // If the event is not dispatched by the end of the timeout,
       // the user probably doesn't have MetaMask installed.
+      console.log('timeout');
       setTimeout(this.handleEthereum, 3000); // 3 seconds
     }
   }
